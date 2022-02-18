4e20acd93db91eb72a08d6392d77ea63c5efe527
sakataku1991 <sakataku1991@gmail.com>
2022-02-19 08:15:12 +0900

    「before-login.vue」ファイルを作成

---
+++ components/BeforeLogin/BeforeLoginAppBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 「app」とレイアウトファイル側の「v-main」はワンセット！ -->
+  <!-- ワンセットになっていることで、固定ツールバー分のマージンが確保されるっぽい -->
+  <!-- Doc: https://vuetifyjs.com/ja/api/v-app-bar/ -->
+  <v-app-bar
+    app
+    :height="homeAppBarHeight"
+    color="white"
+  >
+    <nuxt-link
+      to="/"
+      class="text-decoration-none"
+    >
+      <app-logo />
+    </nuxt-link>
+
+    <app-title
+      class="hidden-mobile-and-down"
+    />
+
+    <v-spacer />
+
+    <app-signup-button />
+    <app-login-button />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data ({ $store }) {
+    return {
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  }
+}
+</script>
